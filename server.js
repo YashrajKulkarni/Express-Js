83277602f1c25247d585e5b29713330f4c95f909
YashrajKulkarni <yashrajkulkarni2001@gmail.com>
2022-07-15 19:42:53 +0530

    t2

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express')
 
-const app = express()
+const app = express();
 
-app.listen(3000);
+const PORT = 3000;
+
+const path = require('path')
+
+app.use(express.static('public'))
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.resolve(__dirname) + '/index.html')
+// })
+// app.get('/about', (req,res)=>{
+//     res.sendFile(path.resolve(__dirname) + '/about.html')
+// })
+
+
+app.listen(PORT, () => {     console.log(`listening on port ${PORT}`)
+});
+
+30:12
+
